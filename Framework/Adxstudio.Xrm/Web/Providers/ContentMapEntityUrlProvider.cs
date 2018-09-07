662f7dd420aac74e4492c9f0dc84a6ac7b3346a0
Alan Mervitz <amervitz@users.noreply.github.com>
2018-09-07 16:22:21 -0600

    Make spacing consistent (use tabs instead of spaces)

--- Framework/Adxstudio.Xrm/Web/Providers/ContentMapEntityUrlProvider.cs
+++ Framework/Adxstudio.Xrm/Web/Providers/ContentMapEntityUrlProvider.cs
@@ -53,8 +53,8 @@ namespace Adxstudio.Xrm.Web.Providers
 			switch (entity.LogicalName)
 			{
 				case "adx_weblink":
-                    return string.IsNullOrWhiteSpace(path.ExternalUrl) ? ContextLanguageInfo.PrependLanguageCode(path) : path;
-                case "adx_webpage":
+					return string.IsNullOrWhiteSpace(path.ExternalUrl) ? ContextLanguageInfo.PrependLanguageCode(path) : path;
+				case "adx_webpage":
 					return ContextLanguageInfo.PrependLanguageCode(path);
 			}
 
